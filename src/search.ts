5dfbc0d6daec480dcdf358d48e3d56f73d186e97
hsh5206 <about5206@naver.com>
2022-04-01 00:00:51 +0900

    Add Search Function

--- src/search.ts
+++ src/search.ts
@@ -1,42 +1,43 @@
 import TMDB from './service/tmdb_api'
 
 export default class Search {
-  constructor(){
+  tmdb: TMDB
+  private results: any
+  constructor() {
     this.tmdb = new TMDB(process.env.TMDB_API_KEY)
-    const query = document.querySelector('.search-input')
+    const query = document.querySelector('.search-input')! as HTMLInputElement
     query.addEventListener('keyup', () => {
-      this.tmdb.getSearchResults(`${query.value}`)
-      .then((result)=>this.results = result)
-      .then(()=>this.renderPage())
+      this.tmdb
+        .getSearchResults(`${query.value}`)
+        .then((result) => (this.results = result))
+        .then(() => this.renderPage())
     })
-    
   }
 
-  renderPage= () => {
-    console.log(document.querySelector('.search').style.display)
-    if (!document.querySelector('.search').style.display) return
+  renderPage = () => {
+    const display = document.querySelector('.search')! as HTMLElement
+    if (!display.style.display) return
     if (!this.results) return
     let temp = `<ul class="search-results">`
     const width = document.body.clientWidth
     const division = width > 600 ? 220 : 170
-    let num = parseInt(width / division)
+    let num = Number(width / division)
     let k = -1
-    this.results.map((movie) => {
+    this.results.map((movie: any) => {
       k++
       if (k > num) {
         temp += `</ul>`
         k = 0
         temp += `<ul class="search-results">`
       }
-      if(!movie.poster_path){
+      if (!movie.poster_path) {
         temp += `
       <li class="search-movie">
         <img class="search-movie-poster" src="assets/images/defaultPoster.jpeg"/>
         <div class="search-movie-title">${movie.title}</div>
       </li>
       `
-      }
-      else{
+      } else {
         temp += `
       <li class="search-movie">
         <img class="search-movie-poster" src="https://image.tmdb.org/t/p/w200${movie.poster_path}"/>
@@ -44,13 +45,16 @@ export default class Search {
       </li>
       `
       }
-      
     })
     temp += '</ul>'
-    const searchResults = document.querySelector('.search-content')
+    const searchResults = document.querySelector(
+      '.search-content'
+    )! as HTMLElement
     searchResults.innerHTML = temp
-    const w = width>600?150:100
-    const h = width>600?225:150
-    for(k;k<num;k++)searchResults.lastElementChild.innerHTML += `<div style="padding:5px; display:block; width:${w}px; height:${h}px"></div>`
+    const w = width > 600 ? 150 : 100
+    const h = width > 600 ? 225 : 150
+    const searchResultsChild = searchResults.lastElementChild! as HTMLElement
+    for (k; k < num - 1; k++)
+      searchResultsChild.innerHTML += `<div style="padding:5px; display:block; width:${w}px; height:${h}px"></div>`
   }
 }
